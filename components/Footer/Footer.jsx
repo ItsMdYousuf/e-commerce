210aea8cfa34daf7ae1ef58c0e7f77ffd9a6fb1a
yousufislamme <online.okk@gmail.com>
2024-11-06 23:01:04 +0600

    Adding Footer #5

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 const Footer = () => {
   return (
-    <p className="text-center">Copyright by E-Commerce All Rights Reserved</p>
+    <p className="text-center text-sm">
+      Copyright by E-Commerce All Rights Reserved
+    </p>
   );
 };
 
